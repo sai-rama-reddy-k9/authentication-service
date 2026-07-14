658b90a7cf5b133e35d25006d1d4704fb650814f
k v sai rama reddy <venkatasairamareddy64@gmail.com>
2026-07-14 15:10:56 +0530

    changed frontend url form localhost to acutal url

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,12 @@ const app = express();
 
 app.use(express.json());
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://authentication-service-frontend.onrender.com",
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
